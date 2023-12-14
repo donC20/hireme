515f595bd9bf6e6c403f1aa552a2eb81840c6bbd
Don Benny <donbenny916@gmail.com>
2023-12-14 17:34:58 +0530

    main server change

---
+++ server/connect.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('dotenv').config()
+// exress app
+const app=express();
+
+// Respond to request
+app.get('/',(req,res)=>{
+    res.json({msg:"Hello World"});
+})
+
+// listen for request
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});

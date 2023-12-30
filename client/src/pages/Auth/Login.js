11a7d24c82670fa200269ea1e61dc8d0e17ec7f8
Don Benny <donbenny916@gmail.com>
2023-12-31 02:05:06 +0530

    https://hireme-api-97hy.onrender.com

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -60,7 +60,7 @@ const Login = () => {
         };
 
         try {
-            const response = await fetch('/api/jobposts/JDIEodHDFND93DKW-KJSJJFJKSMs345k/userAuthenticate', {
+            const response = await fetch('https://hireme-api-97hy.onrender.com/api/jobposts/JDIEodHDFND93DKW-KJSJJFJKSMs345k/userAuthenticate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',

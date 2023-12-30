84a133abd072879f98893366270797f05dadb3f0
Don Benny <donbenny916@gmail.com>
2023-12-30 21:18:45 +0530

    failed

--- server/index.js
+++ server/index.js
@@ -1,62 +1,62 @@
-// const express = require('express');
-// const session = require('express-session');
-// const cors = require('cors');
-// // const cookieParser = require('cookie-parser');
-// require('dotenv').config()
-// // exress app
-// const app = express();
-// // mongoose mongoDB connection
-// const mongoose = require('mongoose');
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+require('dotenv').config()
+// exress app
+const app = express();
+// mongoose mongoDB connection
+const mongoose = require('mongoose');
 
-// // route initialization
-// const jobRoutes = require('./routes/job_routes');
+// route initialization
+const jobRoutes = require('./routes/job_routes');
 
-// // Express -- middleware
-// app.use(express.json());
+// Express -- middleware
+app.use(express.json());
 
-// // cors
-// const origin =
-//     process.env.NODE_ENV === 'production'
-//         ? 'https://hireme-nu.vercel.app'
-//         : 'http://localhost:3000';
+// cors
+const origin =
+    process.env.NODE_ENV === 'production'
+        ? 'https://hireme-nu.vercel.app'
+        : 'http://localhost:3000';
 
-// app.use(cors({
-//     origin,
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-// }));
-
-
-
-// // session
-// // app.use(cookieParser());
-// app.use(session({
-//     secret: 'session-secret',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         secure: false,
-//         maxAge: 1000 * 60 * 60 * 24
-//     }
-// }));
+app.use(cors({
+    origin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 
-// // Global moddleware
-// app.use((req, res, next) => {
-//     console.log(req.url, req.method);
-//     console.log('session started Hello');
-//     // res.json({msg:"Hello connected"})
-//     next();
-// })
-// // routes
-// app.use('/api/jobposts', jobRoutes);
+
+// session
+// app.use(cookieParser());
+app.use(session({
+    secret: 'session-secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
 
 
-// mongoose.connect(process.env.MONGO_URI).then(() => {
-//     // listen for request
-//     const PORT = process.env.PORT;
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port ${PORT}`);
-//     });
-// }).catch((err) => { console.log(err) })
+// Global moddleware
+app.use((req, res, next) => {
+    console.log(req.url, req.method);
+    console.log('session started Hello');
+    // res.json({msg:"Hello connected"})
+    next();
+})
+// routes
+app.use('/api/jobposts', jobRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    // listen for request
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}).catch((err) => { console.log(err) })
 

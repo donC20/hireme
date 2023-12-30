b0eadf014636a6c71f2b0223028e8c134dbc74c2
Don Benny <donbenny916@gmail.com>
2023-12-30 18:05:30 +0530

    error ffinder

--- server/connectCollection.js
+++ server/connectCollection.js
@@ -1,7 +1,7 @@
 // Connect to MongoDB
 const mongoose = require('mongoose');
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error for hireme:'));
 db.once('open', () => {
     console.log('Connected to MongoDB');
 });

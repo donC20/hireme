f5d6a75a7935da987cba02ac087f9dbfe8f719f1
Don Benny <donbenny916@gmail.com>
2023-12-30 23:42:17 +0530

    dsaw

--- server/index.js
+++ server/index.js
@@ -54,12 +54,6 @@ app.use((req, res, next) => {
     console.log('session started Hello');
     // res.send('Yea we are in!')
     // res.json({msg:"Hello connected"})
-    res.set({
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Origin': 'https://hireme-nu.vercel.app',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        'Access-Control-Allow-Headers': 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-    });
     next();
 })
 // routes
